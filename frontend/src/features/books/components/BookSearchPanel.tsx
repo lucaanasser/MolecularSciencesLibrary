d0c95a48467411bf47d6e7d1ca14e6361233998a
lucaanasser <lucamarinhonasser@gmail.com>
2025-08-06 01:57:46 -0300

    Melhorias esteticas v27

--- frontend/src/features/books/components/BookSearchPanel.tsx
+++ frontend/src/features/books/components/BookSearchPanel.tsx
@@ -163,7 +163,7 @@ const BookSearch: React.FC = () => {
               {groupedBooks.map(book => (
                 <div
                   key={book.code}
-                  className="bg-cyan-50/10 rounded-2xl p-4 shadow-lg border border-gray-200"
+                  className="bg-cyan-50/50 rounded-2xl p-4 shadow-lg border border-gray-200"
                 >
                   <div className="flex justify-between items-start">
                     <div>
