c3ef165523b1e382c2f6683500fd278a1809c4e4
lucaanasser <lucamarinhonasser@gmail.com>
2026-01-13 15:33:03 -0300

    feat: adiciona barrel export dos componentes da grade

---
+++ frontend/src/features/grade/components/index.ts
@@ -0,0 +1,11 @@
+export { GradeGrid } from './GradeGrid';
+export { DisciplineSearch } from './DisciplineSearch';
+export { PlanTabs } from './PlanTabs';
+export { AddCustomDiscipline } from './AddCustomDiscipline';
+export { CreditsCounter } from './CreditsCounter';
+export { DisciplineList } from './DisciplineList';
+export { DisciplineListNew } from './DisciplineListNew';
+export { CombinationSelector } from './CombinationSelector';
+export { MiniGradeCombinations } from './MiniGradeCombinations';
+export { DisciplineFilters } from './DisciplineFilters';
+
