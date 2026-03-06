35dac8cf404090366ba908ebb983d5e8e4adcec5
lucaanasser <lucamarinhonasser@gmail.com>
2026-03-06 17:18:11 -0300

    admin: exporta PendingUsersTable para uso na aba

--- frontend/src/features/admin/features/users/index.tsx
+++ frontend/src/features/admin/features/users/index.tsx
@@ -2,3 +2,4 @@ export {default as AddUser } from './AddUserForm';
 export {default as ImportUsers } from './ImportUsers';
 export {default as ListUsers } from './ListUsers';
 export {default as RemoveUser } from './RemoveUserForm';
+export {default as PendingUsersTable } from './PendingUsersTable';
